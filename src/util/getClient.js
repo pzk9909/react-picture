5706890ff4808cf3761cd35a609920940eb81888
zhikai.peng <pzk9909@163.com>
2022-07-27 10:07:20 +0800

    last

--- src/util/getClient.js
+++ src/util/getClient.js
@@ -1,15 +1,15 @@
 export function getClientHeight(){
-    var clientHeight = 0;
+    let clientHeight = 0;
     if (document.body.clientHeight && document.documentElement.clientHeight) {
-        var clientHeight = (document.body.clientHeight < document.documentElement.clientHeight) ? document.body.clientHeight : document.documentElement.clientHeight;
+         clientHeight = (document.body.clientHeight < document.documentElement.clientHeight) ? document.body.clientHeight : document.documentElement.clientHeight;
     }
     else {
-        var clientHeight = (document.body.clientHeight > document.documentElement.clientHeight) ? document.body.clientHeight : document.documentElement.clientHeight;
+         clientHeight = (document.body.clientHeight > document.documentElement.clientHeight) ? document.body.clientHeight : document.documentElement.clientHeight;
     }
     return clientHeight;
 }
 
 export function getClientWidth(){
-    var clientWidth = document.documentElement.clientWidth || document.body.clientWidth;
+    let clientWidth = document.documentElement.clientWidth || document.body.clientWidth;
     return clientWidth
 }
